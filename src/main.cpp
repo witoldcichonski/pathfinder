7337a829f583ea9fcaf05d48d4bfc073e1c3a135
witoldcichonski <50293671+witoldcichonski@users.noreply.github.com>
2019-05-29 21:20:25 +0200

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main()
 
     PathFinder i;
 
-    i.setInputFileName("/home/witek/Documents/programowanie/gtest-demo/test/testFiles/correctMaze.txt");
+    i.setInputFileName("input.txt");
     i.setOutputFileName("output.txt");
 
     if(i.getInputData()== false)
